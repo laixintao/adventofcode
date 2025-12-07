d5fe92b969b2ed1c93850a1a0d6b413161967354
laixintao <laixintaoo@gmail.com>
2025-12-07 17:13:31 +0800

    d7p2

--- 2025/day7/main.c
+++ 2025/day7/main.c
@@ -4,6 +4,35 @@
 
 #define LINE_BUFFER_SIZE 1024
 
+int total_row = 0;
+long long cache[2000][1000];
+
+long long count_timeline(char (*arr)[LINE_BUFFER_SIZE], int row, int i) {
+  if (row == total_row - 2) {
+    return 1;
+  }
+
+  if (cache[row][i] != -1) {
+    return cache[row][i];
+  }
+
+  char *line = arr[row + 2];
+
+  long long timeline = 0;
+
+  if (line[i] == '^') {
+    timeline += count_timeline(arr, row + 2, i - 1);
+    timeline += count_timeline(arr, row + 2, i + 1);
+  } else {
+    timeline += count_timeline(arr, row + 2, i);
+  }
+  printf("%*s", row, "");
+  printf("row = %d, i=%d, total=%lld\n", row, i, timeline);
+  cache[row][i] = timeline;
+
+  return timeline;
+}
+
 int compute(char *filename) {
   FILE *fp = fopen(filename, "r");
   if (!fp) {
@@ -11,32 +40,25 @@ int compute(char *filename) {
     return 1;
   }
   char buffer[200][LINE_BUFFER_SIZE];
-  int col = 0, row = 0;
+  int row = 0;
   int split = 0;
 
   while (fgets(buffer[row], sizeof(buffer), fp)) {
+    total_row += 1;
     buffer[row][strcspn(buffer[row], "\n")] = '\0';
-    char *line = buffer[row];
-    col = strlen(line);
-    for (int i = 0; line[i] != '\0'; i++) {
-      if (line[i] == '.' && row - 1 >= 0 &&
-          (buffer[row - 1][i] == 'S' || buffer[row - 1][i] == '|')) {
-        line[i] = '|';
-      }
-      if (line[i] == '^' && (buffer[row - 1][i] == '|')) {
-        split++;
-        if (i - 1 >= 0)
-          line[i - 1] = '|';
-        if (i + 1 < col)
-          line[i + 1] = '|';
-      }
-    }
-    printf("line: %s\n", line);
-
     row++;
   }
+  printf("total rows: %d \n", total_row);
 
-  printf("split: %d\n", split);
+  memset(cache, -1, sizeof(cache));
+
+  long long timeline = 0;
+  for (int i = 0; i < strlen(buffer[0]); i++) {
+    if (buffer[0][i] == 'S') {
+      timeline += count_timeline(buffer, 0, i);
+    }
+  }
+  printf("timeline: %lld\n", timeline);
   return 0;
 }
 
